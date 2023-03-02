472e4c7215a11e61980f1cd1cbbef68e014b4151
DiegoMP-235 <121039161@upq.edu.mx>
2023-03-01 22:31:17 -0600

    Se corrigieron las identaciones para poder hacer uso de los metodos de la clase

--- Usuario.py
+++ Usuario.py
@@ -1,20 +1,20 @@
 class Usuario:
-     #Constructor
-     
-   def  __init__(self):
+     #Constructor   
+    def  __init__(self):
         self.__correo = "adminfrommylogin@ump.com" 
         self.__password = "ÑandúG@ming235"
-    
+ 
     #Declaramos metodos SETTER y GETTER
-        def setCorreo(self,Correo):
-            self.__correo=Correo  
+    def setCorreo(self,Correo):
+        self.__correo = Correo 
+    
+    def setPassword(self,Password):
+        self.__password = Password
         
-        def setPassword(self,Password):
-            self.__password=Password
-        
-        def getCorreo(self):
-            return self.__correo
-        
-        def getPassword(self):
-            return self.__password    
+    def getCorreo(self):
+        return self.__correo
+    
+    def getPassword(self):
+        return self.__password          
+
                 
