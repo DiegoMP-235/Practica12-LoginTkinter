d2a9ba8c8826e3f353a8d3793ac6caa42c543ccf
DiegoMP-235 <121039161@upq.edu.mx>
2023-03-03 14:15:02 -0600

    Se cambio el metodo de posicionamiento de los widget de entrada
    Se cambio el método de posicionamiento de los elementos del formulario(label y entry)

--- Login.py
+++ Login.py
@@ -4,7 +4,6 @@ from tkinter import Tk,Frame,Entry,Label,Button,messagebox
 #Para mostrar lo contraseña como oculta
 #entrada = Entry(show="*")
 def obtenCorreotxt():
-    #print(inputCorreo.get())
     return inputCorreo.get()
 
 def obtenPasswordtxt():
@@ -32,6 +31,7 @@ CaracterOculto = "■"
 ventana = Tk()
 ventana.title("Login!")
 ventana.geometry("380x250")
+ventana.resizable(False,False)
 
 #Instanciamos Frame
 Principal = Frame(ventana,bg="#c8faa5")
@@ -39,7 +39,8 @@ Principal.pack(expand=True,fill="both")
 
 #Seccion donde iran etiquetas y entradas de texto
 FrameInputs = Frame(Principal,bg="#ada4a3")
-FrameInputs.pack(expand=True,fill="both")
+FrameInputs.pack(expand=True,fill="both",pady=20)
+
 #Seccion donde iran los botones
 FrameButton = Frame(Principal,bg="#2e60e8")
 FrameButton.pack(expand=True,fill="both")
@@ -47,22 +48,25 @@ FrameButton.pack(expand=True,fill="both")
 #Instanciamos entrada de texto y etiquetas
 #Campo para el correo
 labelCorreo = Label(FrameInputs,text="Correo:")
-labelCorreo.grid(row = 0,column = 0)
+labelCorreo.place(x=0,y=0,width=75,height=20)
 
-inputCorreo = Entry(FrameInputs,width=35)
-inputCorreo.grid(row=0,column=1)
+inputCorreo = Entry(FrameInputs,width=35) 
+inputCorreo.place(x=85,y=0,width=200,height=20)
+
 
 #Campo para la password
 labelPassword = Label(FrameInputs,text="Contraseña:")
-labelPassword.grid(row = 1,column = 0)
+#labelPassword.grid(row = 1,column = 0)
+labelPassword.place(x=0,y=40,width=75,height=20)
+
 
 inputPassword = Entry(FrameInputs,show=CaracterOculto,width=25)
-inputPassword.grid(row=1,column=1)
+inputPassword.place(x=85,y=40,width=200,height=20)
 
 #Dejamos el correo por defecto para que pueda ingresar
 inputCorreo.insert(0,myUsuer.getCorreo())
 #Para el boton 
-btnIngresar = Button(FrameButton,text="Ingresar",bg="#0dff21",command=ingresar)
+btnIngresar = Button(FrameButton,text="Ingresar",bg="#0dff33",command=ingresar)
 btnIngresar.pack()
 
 #mostramos la ventana
