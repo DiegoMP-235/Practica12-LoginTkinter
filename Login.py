638d64008c603b6554a34babde81a788f170a2ae
DiegoMP-235 <121039161@upq.edu.mx>
2023-03-03 16:38:43 -0600

    Se eliminaron los métodos de validación
    Se pasaron los métodos de validación a la clase de "UtileriaLogin" , solo se agrego una funcion que fungira como submit para enviar los datos a las funciones para validar

--- Login.py
+++ Login.py
@@ -1,4 +1,5 @@
 from Usuario import *
+from UtileriaLogin import *
 from tkinter import Tk,Frame,Entry,Label,Button,messagebox  
 #https://recursospython.com/guias-y-manuales/caja-de-texto-entry-tkinter/
 #Para mostrar lo contraseña como oculta
@@ -8,24 +9,13 @@ def obtenCorreotxt():
 
 def obtenPasswordtxt():
     return inputPassword.get()    
-
-def validaCamposNoVacios():
-    return(inputCorreo.get() != "" and inputPassword.get() != "")
-
-def compruebaCredenciales(Correo,Password):
-    return (myUsuer.getCorreo()==Correo and myUsuer.getPassword()==Password)
-
-def ingresar():
-    if(validaCamposNoVacios()):
-        if(compruebaCredenciales(obtenCorreotxt(),obtenPasswordtxt())): #En caso de que las credenciales coincidan
-            messagebox.showinfo("Inicio exitoso!","Bienvenido al sistema!")
-        else: #En caso de que las credenciales no coincidan
-            messagebox.showerror("Ha ocurrido un error :/","Las claves de acceso no coinciden")    
-    else:
-        messagebox.showwarning("Faltan campos","Por favor completa todos los campos")    
+def iniciarSesion():
+    inicioSesion.ingresar(obtenCorreotxt(),obtenPasswordtxt(),myUsuer.getCorreo(),myUsuer.getPassword())
     
 #Instanciamos objeto tipo Usuario
+COLOR="#e0d6bf"
 myUsuer = Usuario()
+inicioSesion = UtileriaLogin()
 CaracterOculto = "■"
 #Instanciamos la ventana
 ventana = Tk()
@@ -34,15 +24,15 @@ ventana.geometry("380x250")
 ventana.resizable(False,False)
 
 #Instanciamos Frame
-Principal = Frame(ventana,bg="#c8faa5")
+Principal = Frame(ventana,bg=COLOR)
 Principal.pack(expand=True,fill="both")
 
 #Seccion donde iran etiquetas y entradas de texto
-FrameInputs = Frame(Principal,bg="#ada4a3")
+FrameInputs = Frame(Principal,bg=COLOR)
 FrameInputs.pack(expand=True,fill="both",pady=20)
 
 #Seccion donde iran los botones
-FrameButton = Frame(Principal,bg="#2e60e8")
+FrameButton = Frame(Principal,bg=COLOR)
 FrameButton.pack(expand=True,fill="both")
 
 #Instanciamos entrada de texto y etiquetas
@@ -66,7 +56,7 @@ inputPassword.place(x=85,y=40,width=200,height=20)
 #Dejamos el correo por defecto para que pueda ingresar
 inputCorreo.insert(0,myUsuer.getCorreo())
 #Para el boton 
-btnIngresar = Button(FrameButton,text="Ingresar",bg="#0dff33",command=ingresar)
+btnIngresar = Button(FrameButton,text="Ingresar",bg="#0dff33",command=iniciarSesion)
 btnIngresar.pack()
 
 #mostramos la ventana
